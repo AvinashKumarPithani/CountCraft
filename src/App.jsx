9b229af4fc88d0f35fe74325c03a3319561ac466
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2024-08-22 19:54:20 +0530

    Implementing Counter using state

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,24 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let counter = 26;
+
+  const [counter, setCounter] = useState(26)
+  // let counter = 26;
 
   const addValue = () => {
-    counter = counter + 1;
-    console.log(counter);
+    setCounter(counter + 1)
   };
+  const removeValue = () => {
+    setCounter(counter - 1)
+  }
 
   return (
     <>
-      <h1>Counts Craft</h1>
+      <h1>Count Craft {counter}</h1>
       <h2>Counter Value : {counter} </h2>
       <button onClick={addValue}>Add Value</button>{" "}
-      <button>Remove Value</button>
-      <p>footer: </p>
+      <button onClick={removeValue}>Remove Value</button>
+      <p>footer : {counter}</p>
     </>
   );
 }
